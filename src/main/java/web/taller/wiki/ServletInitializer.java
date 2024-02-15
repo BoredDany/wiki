632cce725afd468bda398cb60c2183d34f5a5b46
BoredDany <martinezq.d@javeriana.edu.co>
2024-02-15 11:22:48 -0500

    first commit

---
+++ src/main/java/web/taller/wiki/ServletInitializer.java
@@ -0,0 +1,13 @@
+package web.taller.wiki;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WikiApplication.class);
+	}
+
+}
